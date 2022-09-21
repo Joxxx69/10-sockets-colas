8797c30067513472f26522e710df95118af413bc
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-09-20 23:33:45 -0500

    cambios

--- sockets/controller.sockets.js
+++ sockets/controller.sockets.js
@@ -4,23 +4,20 @@ const ticketControl = new TicketControl();
 
 
 const socketController = (socket) => {
-    
-    console.log('Cliente conectado', socket.id );
-
+    console.log('Cliente Conectado',{socket},socket.id);
     socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id );
+        console.log('Cliente Desconectado',socket.id);
+    });
+    
+    socket.on('enviar-mensaje', (payload,callback) => {
+        const id = 1234;
+        console.log(payload);
+        callback(id);
+        socket.broadcast.emit('enviar-mensaje-todos',payload)
     });
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
-
-        socket.broadcast.emit('enviar-mensaje-todos', payload );
-
-    })
-
 }
+
 
 module.exports = {
     socketController
